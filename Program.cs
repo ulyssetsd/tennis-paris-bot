7f0bd7d68f4b9c8941d4eeead922d79db35e8419
ulyssetsd <utassidis@hotmail.fr>
2023-06-10 15:19:51 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,50 +1,59 @@
-﻿using Microsoft.Playwright;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
 
-var tennisName = "Léo Lagrange";
-var dayOfWeek = "Monday";
-var hourOfDay = "13h";
-var email = "email@gmail.com";
-var password = "**********";
-var secondPlayerLastName = "Smith";
-var secondPlayerFirstName = "Will";
+namespace TennisParisBot;
 
-using var playwright = await Playwright.CreateAsync();
-await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+static class Program
 {
-    Headless = false,
-});
-var page = await browser.NewPageAsync();
+    static async Task Main(string email, string password)
+    {
+        var tennisName = "Léo Lagrange";
+        var dayOfWeek = "Monday";
+        var hourOfDay = "13h";
+        //var email = "email@gmail.com";
+        //var password = "**********";
+        var secondPlayerLastName = "Smith";
+        var secondPlayerFirstName = "Will";
 
-await page.GotoAsync("https://tennis.paris.fr/tennis/jsp/site/Portal.jsp?page=tennis&view=startDefault&full=1");
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+            SlowMo = 50,
+        });
+        var page = await browser.NewPageAsync();
 
-var loginPage = await page.RunAndWaitForPopupAsync(async () =>
-{
-    await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Je me connecte" }).ClickAsync();
-});
+        await page.GotoAsync("https://tennis.paris.fr/tennis/jsp/site/Portal.jsp?page=tennis&view=startDefault&full=1");
 
-await loginPage.GetByPlaceholder("Votre adresse électronique").FillAsync(email);
-await loginPage.GetByPlaceholder("Votre mot de passe").FillAsync(password);
-await loginPage.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Se connecter" }).ClickAsync();
+        var loginPage = await page.RunAndWaitForPopupAsync(async () =>
+        {
+            await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Je me connecte" }).ClickAsync();
+        });
 
-await page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "accueil" }).ClickAsync();
-await page.Locator("#whereToken").GetByRole(AriaRole.Textbox).ClickAsync();
-await page.Locator("#whereToken").GetByRole(AriaRole.Textbox).FillAsync(tennisName);
-await page.GetByRole(AriaRole.Strong).Filter(new LocatorFilterOptions { HasTextRegex = new Regex($"^{tennisName}$") }).ClickAsync();
-await page.GetByPlaceholder("Sélectionnner une date").ClickAsync();
-await page.Locator("#search_form").GetByText(new Regex($"^{dayOfWeek}*.")).ClickAsync();
-await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Rechercher" }).ClickAsync();
-await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = hourOfDay }).ClickAsync();
-await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Réserver" }).First.ClickAsync();
+        await loginPage.GetByPlaceholder("Votre adresse électronique").FillAsync(email);
+        await loginPage.GetByPlaceholder("Votre mot de passe").FillAsync(password);
+        await loginPage.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Se connecter" }).ClickAsync();
 
-await page.FrameLocator("iframe[name=\"captchaImage\"]").GetByRole(AriaRole.Img).ClickAsync();
-await page.GetByLabel("* Enter the heard or read characters :").FillAsync("vqMYIO");
-await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Poursuivre la réservation" }).ClickAsync();
+        await page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "accueil" }).ClickAsync();
+        await page.Locator("#whereToken").GetByRole(AriaRole.Textbox).ClickAsync();
+        await page.Locator("#whereToken").GetByRole(AriaRole.Textbox).FillAsync(tennisName);
+        await page.GetByRole(AriaRole.Strong).Filter(new LocatorFilterOptions { HasTextRegex = new Regex($"^{tennisName}$") }).ClickAsync();
+        await page.GetByPlaceholder("Sélectionnner une date").ClickAsync();
+        await page.Locator("#search_form").GetByText(new Regex($"^{dayOfWeek}*.")).ClickAsync();
+        await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Rechercher" }).ClickAsync();
+        await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = hourOfDay }).ClickAsync();
+        await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Réserver" }).First.ClickAsync();
 
-await page.GetByRole(AriaRole.Textbox).First.FillAsync(secondPlayerLastName);
-await page.GetByRole(AriaRole.Textbox).Nth(1).FillAsync(secondPlayerFirstName);
-await page.Locator("body").ClickAsync();
-await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Etape suivante" }).ClickAsync();
-await page.GetByRole(AriaRole.Cell, new PageGetByRoleOptions { NameRegex = new Regex("J’utilise . heure de mon carnet .*") }).ClickAsync();
-await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Etape suivante" }).ClickAsync();
-await page.GetByText("Réservation confirmée").ClickAsync();
+        await page.FrameLocator("iframe[name=\"captchaImage\"]").GetByRole(AriaRole.Img).ClickAsync();
+        await page.GetByLabel("* Enter the heard or read characters :").FillAsync("vqMYIO");
+        await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Poursuivre la réservation" }).ClickAsync();
+
+        await page.GetByRole(AriaRole.Textbox).First.FillAsync(secondPlayerLastName);
+        await page.GetByRole(AriaRole.Textbox).Nth(1).FillAsync(secondPlayerFirstName);
+        await page.Locator("body").ClickAsync();
+        await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Etape suivante" }).ClickAsync();
+        await page.GetByRole(AriaRole.Cell, new PageGetByRoleOptions { NameRegex = new Regex("J’utilise . heure de mon carnet .*") }).ClickAsync();
+        await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Etape suivante" }).ClickAsync();
+        await page.GetByText("Réservation confirmée").ClickAsync();
+    }
+}
